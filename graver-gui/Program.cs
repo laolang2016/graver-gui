21430fb78b88aaa20b0c9a9d33508c847cdc3885
laolang2016 <xiaodaima2016@163.com>
2023-06-07 00:20:52 +0800

    🎉 init: 初始化项目

---
+++ graver-gui/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graver_gui
+{
+    internal static class Program
+    {
+        private static readonly NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            log.Info("hello world");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
